a545f5116bd20314ed75b7e208b977879da66dda
msermersheim <m.sermersheim@gmail.com>
2017-01-14 12:41:41 -0800

    Progress on db queries, working on get req

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,13 +2,34 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) { console.log('lets make jon mad1.'); }, // a function which handles a get request for all messages
-    post: function (req, res) { console.log('lets make jon mad.'); } // a function which handles posting a message to the database
+    get: function (req, res) { 
+
+      // Go to model and fetch messages
+
+      // send messages as JSON 
+
+      
+
+      var ourGreeting = models.messages.get();
+
+      res.send(ourGreeting);
+
+    }, // a function which handles a get request for all messages
+    post: function (req, res) { 
+
+      // send data to model post method. 
+
+      var data = models.messages.post();
+
+      res.send('Hello World!');
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) { console.log('lets make jon mad2.'); },
+    get: function (req, res) { 
+      res.send('inside GET user controller!');
+    },
     post: function (req, res) { console.log('lets make jon mad3.'); } 
   }
 };
--- server/models/index.js
+++ server/models/index.js
@@ -1,14 +1,37 @@
-var db = require('../db');
+var con = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function () {
+
+      var data;
+
+      con.query('SELECT * FROM messages', function(error, result) {
+        if (error) {
+          //return error;
+          //throw error;
+          console.log('ERROR:', error);
+        }
+        //return rows;
+        console.log('RESULT:', result);
+        console.log('MESSAGE:', result[0].message);
+
+
+        data = result[0].message;
+        
+      });
+
+      return 'HELLO';
+
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
+    get: function () {
+      console.log('does this work?');
+    },
     post: function () {}
   }
 };
